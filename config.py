e03e66a8d4db63a642425724365029c16ea2795a
dev-harsh-codes <rajharsh62037@gmail.com>
2026-06-18 05:44:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,4 +24,4 @@ MODES = {
 TLS_DOMAIN = "cloudflare.com"
 
 # Tag for advertising, obtainable from @MTProxybot
-# AD_TAG = "3c09c680b76ee91a4c25ad51f742267d"
+AD_TAG = "cded575c304da847fe5378fd42834441"
